c9387e66cdf73d2afc174d546d8ab61c46b81991
Your Name <you@example.com>
2021-02-19 02:16:05 +0600

    home page done wiht 85%

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:business_Model_App/business_model/ui/white_mode/catalogous_for_white.dart';
+//import 'package:business_Model_App/business_model/ui/white_mode/catalogous_for_white.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'business_model/ui/white_mode/home.white.dart';
 
 void main() => runApp(new MaterialApp(
       //theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFEFEFEF)),
-      home: CatalogusPageWhite(),
+      home: HomePage(),
     ));
